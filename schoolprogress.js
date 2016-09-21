56740a32d5fe95a662d0dd2d278430e612fc5e1a
garethjscoleman <garethjscoleman@yahoo.co.uk>
2016-09-21 17:26:51 +0100

    add dynamic style

--- schoolprogress.js
+++ schoolprogress.js
@@ -2,7 +2,8 @@ angular.module('school_progress', ['ui.bootstrap']);
 
 function SchoolProgressCtrl($scope) {
 
- 
+ $scope.cohort=25;
+ $scope.myStyle={'background-color':$scope.score};
 
      $scope.score=0;
   
