5af55e4dc0743276505bc89883325b6d135eb23a
garethjscoleman <garethjscoleman@yahoo.co.uk>
2016-09-21 19:20:53 +0100

    set mystyle

--- schoolprogress.js
+++ schoolprogress.js
@@ -1,12 +1,12 @@
 angular.module('school_progress', ['ui.bootstrap']);
 
 function SchoolProgressCtrl($scope) {
-
+ scope.myStyle={'width':'10%'};
  $scope.cohort=25;
      $scope.score=0;
   
 $scope.$watch('score',function(){
- $scope.myStyle={'width':$scope.score+'%'};
+ $scope.myStyle.width=$scope.score+'%';
  
 });
  
