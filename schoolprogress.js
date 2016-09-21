7546bcc74cc6383a40a3fe757fcca105e18ee298
garethjscoleman <garethjscoleman@yahoo.co.uk>
2016-09-21 19:58:37 +0100

    Update schoolprogress.js

--- schoolprogress.js
+++ schoolprogress.js
@@ -1,6 +1,7 @@
 angular.module('school_progress', ['ui.bootstrap']);
 
 function SchoolProgressCtrl($scope) {
+ $scope.myStyle={'width':'0%'};
  $scope.left25={'width':'2.5%','background-image':'linear-gradient(to right, white, rgb(0,0,0,1))'};
  $scope.left75={'width':'7.5%','background-image':'linear-gradient(to right, rgb(0,0,0,1), rgb(0,0,0,0.8))'};
 $scope.left40={'width':'40%','background-image':'linear-gradient(to right, rgb(0,0,0,0.8), rgb(0,0,0,0.2))'};
