da81b888a169cafc52105a6df0f7b71b93ed0bde
garethjscoleman <garethjscoleman@yahoo.co.uk>
2016-09-21 21:50:08 +0100

    Update schoolprogress.js

--- schoolprogress.js
+++ schoolprogress.js
@@ -5,17 +5,17 @@ function SchoolProgressCtrl($scope) {
  $scope.myStyle2={'width':'50%','float':'left'};
 
  $scope.left25={'width':'2.5%','float':'left','background-image':'linear-gradient(to right, white, rgb(235,235,235))'};
- $scope.left75={'width':'7.5%','float':'left','background-image':'linear-gradient(to right, rgb(235,235,235), rgb(200,200,200))'};
-$scope.left40={'width':'40%','float':'left','background-image':'linear-gradient(to right, rgb(200,200,200), rgb(150,150,150))'};
-$scope.right40={'width':'40%','float':'left','background-image':'linear-gradient(to left, rgb(200,200,200), rgb(150,150,150))'};
-$scope.right75={'width':'7.5%','float':'left','background-image':'linear-gradient(to left, rgb(235,235,235), rgb(200,200,200))'};
+ $scope.left75={'width':'7.5%','float':'left','background-image':'linear-gradient(to right, rgb(235,235,235), rgb(210,210,210))'};
+$scope.left40={'width':'40%','float':'left','background-image':'linear-gradient(to right, rgb(210,210,210), rgb(130,130,130))'};
+$scope.right40={'width':'40%','float':'left','background-image':'linear-gradient(to left, rgb(210,210,210), rgb(130,130,130))'};
+$scope.right75={'width':'7.5%','float':'left','background-image':'linear-gradient(to left, rgb(235,235,235), rgb(210,210,210))'};
 $scope.right25={'width':'2.5%','float':'left','background-image':'linear-gradient(to left, white, rgb(235,235,235))'};
 
 $scope.rleft25={'width':'2.5%','float':'left','background-image':'linear-gradient(to right, white, rgb(235,0,235))'};
- $scope.rleft75={'width':'7.5%','float':'left','background-image':'linear-gradient(to right, rgb(235,0,235), rgb(200,0,200))'};
-$scope.rleft40={'width':'40%','float':'left','background-image':'linear-gradient(to right, rgb(200,0,200), rgb(150,0,150))'};
-$scope.rright40={'width':'40%','float':'left','background-image':'linear-gradient(to left, rgb(200,0,200), rgb(150,0,150))'};
-$scope.rright75={'width':'7.5%','float':'left','background-image':'linear-gradient(to left, rgb(235,0,235), rgb(200,0,200))'};
+ $scope.rleft75={'width':'7.5%','float':'left','background-image':'linear-gradient(to right, rgb(235,0,235), rgb(210,0,210))'};
+$scope.rleft40={'width':'40%','float':'left','background-image':'linear-gradient(to right, rgb(210,0,210), rgb(130,0,130))'};
+$scope.rright40={'width':'40%','float':'left','background-image':'linear-gradient(to left, rgb(210,0,210), rgb(130,0,130))'};
+$scope.rright75={'width':'7.5%','float':'left','background-image':'linear-gradient(to left, rgb(235,0,235), rgb(210,0,210))'};
 $scope.rright25={'width':'2.5%','float':'left','background-image':'linear-gradient(to left, white, rgb(235,0,235))'};
 
  $scope.cohort=25;
