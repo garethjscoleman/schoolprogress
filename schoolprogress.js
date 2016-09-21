99bba248777738235bae5d4264494f9f63e0b0a3
garethjscoleman <garethjscoleman@yahoo.co.uk>
2016-09-21 17:33:28 +0100

    change to set width

--- schoolprogress.js
+++ schoolprogress.js
@@ -3,9 +3,9 @@ angular.module('school_progress', ['ui.bootstrap']);
 function SchoolProgressCtrl($scope) {
 
  $scope.cohort=25;
- $scope.myStyle={'background-color':$scope.score};
-
      $scope.score=0;
-  
+  $scope.myStyle={'width':$scope.score};
+
+ 
 
 }
