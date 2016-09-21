acddf13333519b180b11752d6739b4ce88202e36
garethjscoleman <garethjscoleman@yahoo.co.uk>
2016-09-21 20:48:25 +0100

    Update schoolprogress.js

--- schoolprogress.js
+++ schoolprogress.js
@@ -2,6 +2,8 @@ angular.module('school_progress', ['ui.bootstrap']);
 
 function SchoolProgressCtrl($scope) {
  $scope.myStyle={'width':'0%'};
+ $scope.myStyle2={'width':'50%'};
+
  $scope.left25={'width':'2.5%','float':'left','background-image':'linear-gradient(to right, white, rgb(220,220,220))'};
  $scope.left75={'width':'7.5%','float':'left','background-image':'linear-gradient(to right, rgb(220,220,220), rgb(180,180,180))'};
 $scope.left40={'width':'40%','float':'left','background-image':'linear-gradient(to right, rgb(180,180,180), rgb(150,150,150))'};
@@ -10,10 +12,17 @@ $scope.right75={'width':'7.5%','float':'left','background-image':'linear-gradien
 $scope.right25={'width':'2.5%','float':'left','background-image':'linear-gradient(to left, white, rgb(220,220,220))'};
 
  $scope.cohort=25;
-     $scope.score=0;
+  $scope.score=0;
+
+$scope.$watch('cohort',function(){
+
+var factor = 50*5/Math.sqrt($scope.cohort)
+  $scope.myStyle2.width=factor+'%';
+ 
+});
   
 $scope.$watch('score',function(){
- $scope.myStyle.width=($scope.score+10)+'%';
+ $scope.myStyle.width=(10*($scope.score+5))+'%';
  
 });
  
