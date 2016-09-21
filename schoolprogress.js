0e889201e0d70571e7e97d83813a8c707b0dd131
garethjscoleman <garethjscoleman@yahoo.co.uk>
2016-09-21 20:37:18 +0100

    Update schoolprogress.js

--- schoolprogress.js
+++ schoolprogress.js
@@ -3,10 +3,10 @@ angular.module('school_progress', ['ui.bootstrap']);
 function SchoolProgressCtrl($scope) {
  $scope.myStyle={'width':'0%'};
  $scope.left25={'width':'2.5%','float':'left','background-image':'linear-gradient(to right, white, rgb(220,220,220))'};
- $scope.left75={'width':'7.5%','float':'left','background-image':'linear-gradient(to right, rgb(220,220,220), rgb(150,150,150))'};
-$scope.left40={'width':'40%','float':'left','background-image':'linear-gradient(to right, rgb(150,150,150), rgb(120,120,120))'};
-$scope.right40={'width':'40%','float':'left','background-image':'linear-gradient(to left, rgb(150,150,150), rgb(120,120,120))'};
-$scope.right75={'width':'7.5%','float':'left','background-image':'linear-gradient(to left, rgb(220,220,220), rgb(150,150,150))'};
+ $scope.left75={'width':'7.5%','float':'left','background-image':'linear-gradient(to right, rgb(220,220,220), rgb(180,180,180))'};
+$scope.left40={'width':'40%','float':'left','background-image':'linear-gradient(to right, rgb(180,180,180), rgb(150,150,150))'};
+$scope.right40={'width':'40%','float':'left','background-image':'linear-gradient(to left, rgb(180,180,180), rgb(150,150,150))'};
+$scope.right75={'width':'7.5%','float':'left','background-image':'linear-gradient(to left, rgb(220,220,220), rgb(180,180,180))'};
 $scope.right25={'width':'2.5%','float':'left','background-image':'linear-gradient(to left, white, rgb(220,220,220))'};
 
  $scope.cohort=25;
