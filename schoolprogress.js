b2081547bf4081012ad156d1f2592c7da846143b
garethjscoleman <garethjscoleman@yahoo.co.uk>
2016-09-22 22:59:54 +0100

    Update schoolprogress.js

--- schoolprogress.js
+++ schoolprogress.js
@@ -54,7 +54,7 @@ $scope.$watch('cohort',function(){
 });
   
 $scope.$watch('score',function(){
-  $scope.marginforschoolbarcontainer= (100*(((range+1)/2)-$scope.score)/(range+1))-($scope.widthofschoolbar/2);
+  $scope.marginforschoolbarcontainer= (100*(((range+1)/2)+$scope.score)/(range+1))-($scope.widthofschoolbar/2);
    $scope.myStyle['margin-left']=  (0.5*(100-$scope.marginforschoolbarcontainer))*(($scope.score+((range+1)/2))/((range+1)/2))+'%';
  
 });
